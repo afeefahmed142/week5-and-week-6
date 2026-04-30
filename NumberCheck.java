b9aff7a1373a54026c0a832bea403d0e2ee92eca
afeefahmed142 <afeefahmed142@gmail.com>
2026-04-30 22:02:56 +0530

    coded to level5-2

---
+++ NumberCheck.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        // Input
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        // Check numbers
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] > 0) {
+
+                if (numbers[i] % 2 == 0) {
+                    System.out.println(numbers[i] + " is Positive and Even");
+                }
+                else {
+                    System.out.println(numbers[i] + " is Positive and Odd");
+                }
+            }
+
+            else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is Negative");
+            }
+
+            else {
+                System.out.println(numbers[i] + " is Zero");
+            }
+        }
+
+        // Compare first and last element
+        if (numbers[0] == numbers[4]) {
+            System.out.println("First and last elements are Equal");
+        }
+        else if (numbers[0] > numbers[4]) {
+            System.out.println("First element is Greater than last element");
+        }
+        else {
+            System.out.println("First element is Less than last element");
+        }
+
+        sc.close();
+    }
+}
